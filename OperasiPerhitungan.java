bba9f4cf486e2fab1691c5e5f91d8bf77046a266
khansalaksono <105190559+khansalaksono@users.noreply.github.com>
2022-05-14 09:02:50 +0700

    Create OperasiPerhitungan.java
    Buatlah Operasi berikut dalam bentuk program dalam java. dimana nilai dari a, b, dan c secara berturut turut adalah 3,5,4
    
    a^3 + b / c^2
    
    *Input Format*
    3 5 4
    
    *Constraints*
    Hasil keluaran berupa bilangan Integer
    
    *Output Format*
    2
    
    *Sample Input 0*
    3
    5
    4
    
    *Sample Output 0*
    2
    
    *Sample Input 1*
    0
    1
    1
    
    *Sample Output 1*
    1

---
+++ OperasiPerhitungan.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        
+        int d = (a*a*a);
+        int e = (c*c);
+        int f = (d+b);
+        int g = (f/e);
+        
+        int output = g;
+        
+        System.out.println(output);
+    }
+}
