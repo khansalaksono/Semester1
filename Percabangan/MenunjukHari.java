51decb813cff18f296b75c373daaf1b965e756ef
khansalaksono <105190559+khansalaksono@users.noreply.github.com>
2022-05-14 12:32:45 +0700

    Update and rename MenunjukHari.java to Percabangan/MenunjukHari.java
    https://www.hackerrank.com/contests/tif-g-percabangan-2/challenges/menunjuk-hari/submissions/code/1337536349

--- Percabangan/MenunjukHari.java
+++ Percabangan/MenunjukHari.java
@@ -3,7 +3,7 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class Solution {
+public class MenunjukHari {
     //NAMA  : KHANSA SALSABILA SANGDIVA LAKSONO
     //NIM   : 215150201111068
     public static void main(String[] args) {
